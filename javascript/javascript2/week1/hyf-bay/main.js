8624a3491bcc08b436d1833ab8bdcd763cda2ffa
amjadiftikhar <amjadiftikhar@github.com>
2019-10-05 15:37:41 +0200

    hyf-bay file edited

--- javascript/javascript2/week1/hyf-bay/main.js
+++ javascript/javascript2/week1/hyf-bay/main.js
@@ -1,18 +1,35 @@
 console.log('Script loaded');
 
-// // console.log(getAvailableProducts());
+console.log(getAvailableProducts());
 const products = getAvailableProducts();
-
 //  console.log(products);
-
-function renderProducts(products) { 
-    const productUl = document.querySelector('section.products > ul');
-    const productLi = document.createElement('li');
-    productUl.appendChild(productLi);
-    products.forEach(element => {
-        const newSubUl = document.createElement('ul');
-        productLi.appendChild(newSubUl);
-
+function renderProducts(products) {     
+    const productUl = document.querySelector('section.products > ul');       
+    products.forEach(product => {
+        const productLi = document.createElement('li');
+        productUl.appendChild(productLi); 
+        const newUl =renderProduct(product)        
+        productLi.appendChild(newUl);
     });
 
-}
+function renderProduct(product) {
+    const ul = document.createElement('ul');  
+    const nameLi = document.createElement('li');
+    nameLi.innerText = product.name;    
+    ul.appendChild(nameLi);
+    const rateLi = document.createElement('li');
+    rateLi.innerText = product.rating;    
+    ul.appendChild(rateLi);
+    const priceLi = document.createElement('li');
+    priceLi.innerText = product.price;
+    ul.appendChild(priceLi);
+    const ratingLi = document.createElement('li');
+    ratingLi.innerText = product.rating;
+    ul.appendChild(ratingLi);
+    const shipToLi = document.createElement('li');
+    shipToLi.innerText = product.rating;
+    ul.appendChild(shipToLi);   
+    }  
+} 
+renderProducts(products);
+
