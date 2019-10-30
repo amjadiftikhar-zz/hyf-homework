1d5e90041785a3ec7e2482c6cd44b65a65039ed3
amjadiftikhar <amjadiftikhar@github.com>
2019-10-30 02:04:37 +0100

    promise-visual added to homework

---
+++ javascript/javascript3/week2/promise-visual/main.js
@@ -0,0 +1,27 @@
+// translateOneByOne
+
+function translateOneByOne(){
+    moveElement(document.querySelector('ul.marks li:nth-child(1)'), {x: 20, y: 300})
+    .then(() => { 
+        console.log('redbox has been moved')
+    })
+    .then(() => moveElement(document.querySelector('ul.marks li:nth-child(2)'), {x: 400, y: 300}))
+    .then(() => { console.log('bluebox has been moved')
+    })
+    .then(() => moveElement(document.querySelector('ul.marks li:nth-child(3)'), {x: 400, y: 20}))
+    .then(() => console.log('greenbox has been moved'))    
+}        
+translateOneByOne()    
+
+// translateAllAtOnce
+
+/*
+function translateAllAtOnce(){
+    Promise.all([moveElement(document.querySelector('ul.marks li:nth-child(1)'), {x: 20, y: 300}),
+    moveElement(document.querySelector('ul.marks li:nth-child(2)'), {x: 400, y: 300}),
+    moveElement(document.querySelector('ul.marks li:nth-child(3)'), {x: 400, y: 20})])
+    .then(() => console.log('All boxes have been moved'))
+}        
+translateAllAtOnce()
+
+*/
