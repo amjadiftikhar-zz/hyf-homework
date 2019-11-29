d407d6b7e548cc1080db01a33768372f3d9d46e2
amjadiftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2019-11-29 05:04:59 +0100

    Add files via upload

--- databases/week2/table.sql
+++ databases/week2/table.sql
@@ -1,15 +1,14 @@
--- CREATE SCHEMA `student_db`;
-/* CREATE TABLE `student`
-(`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE SCHEMA `student_db`;
+USE student_db;
+CREATE TABLE `student` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 `name` VARCHAR(255) NOT NULL, 
 `email` VARCHAR(255) NOT NULL, 
 `phone` INT NOT NULL, 
 `class_id` INT NOT NULL,
 PRIMARY KEY(`id`),
-UNIQUE INDEX `name_UNIQUE`(`name` ASC)
+CONSTRAINT `fk_class` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE RESTRICT
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-*/
-
 
 CREATE TABLE `class` (
 `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -18,3 +17,26 @@ CREATE TABLE `class` (
 `ends` DATETIME NOT NULL,
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Create an index on the name column of the student table.
+
+ALTER TABLE student ADD INDEX (name);
+
+-- Add a new column to the class table named status which can only have the following 
+-- values: not-started, ongoing, finished (hint: enumerations).
+
+ALTER TABLE class 
+ADD COLUMN status ENUM('not-started', 'ongoing', 'finished') NOT NULL DEFAULT 'not-started';
+
+
+INSERT INTO `class` (
+`id`, `name`, `begins`, `ends`)
+ VALUES (10, 'class10', '2019-10-10 12:00:00', '2020-12-10 12:00');
+ 
+ -- UPDATE class SET status = 'finished' where id = 8;
+ 
+ 
+INSERT INTO `student` (
+`id`, `name`, `email`, `phone`, `class_id`)
+ VALUES (12, 'Adam Smith', 'smith@bla.com', 769365297, 5);
+ 
--- databases/week2/tasks.sql
+++ databases/week2/tasks.sql
@@ -1,39 +1,45 @@
  -- 1. Add a task with the these attributes: title, description, created, updated, dueDate, statusID, userID
-/* INSERT INTO task (
-title, 
-description, 
-created, 
-updated, 
-due_date, 
-status_id, 
-user_id
-) 
-VALUES
- (
-'write your program', 
-'use your skills', 
-'2019-11-18 23:30:00', 
-'2019-11-18 23:30:00', 
-'2019-12-18 23:30:00',
-2,
-12
-);
- */
+
+INSERT INTO task (
+	title, 
+	description, 
+	created, 
+	updated, 
+	due_date, 
+	status_id, 
+	user_id
+	) 
+	VALUES
+		(
+		'write your program', 
+		'use your skills', 
+		'2019-11-18 23:30:00', 
+		'2019-11-18 23:30:00', 
+		'2019-12-18 23:30:00',
+		2,
+		12
+	);
+	
  
  -- 2. Change the title of a task with these attributes: taskID, newTitle
- -- UPDATE task SET title = 'change clothes' WHERE task.id = 1; 
+ 
+ UPDATE task SET title = 'change clothes' WHERE task.id = 1; 
  
  -- 3. Change the task due date with these attributes: taskID, newDueDate
- -- UPDATE task SET due_date = '2025-10-25 00:00:00' WHERE task.id = 1; 
+ 
+ UPDATE task SET due_date = '2025-10-25 00:00:00' WHERE task.id = 1; 
  
  -- 4. Change the task status with these attributes: taskID, newStatus
- -- UPDATE task SET status_id = 1 WHERE task.id = 1;
+ 
+ UPDATE task SET status_id = 1 WHERE task.id = 1;
  
  -- 5. Mark a task as complete with this attribute: taskID
- -- UPDATE task SET status_id = 3 WHERE task.id = 4;
+ 
+ UPDATE task SET status_id = 3 WHERE task.id = 4;
  
  -- 6. Delete task with this attribute: taskID
- -- DELETE from task WHERE task.id = 10;
+ 
+ DELETE from task WHERE task.id = 10;
  
  
  
