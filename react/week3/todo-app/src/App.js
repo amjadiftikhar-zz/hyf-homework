ca7ad72f0c499417c7b2f45ba933348d888935cc
Amjad Iftikhar <52453740+amjadiftikhar@users.noreply.github.com>
2020-02-14 14:22:55 +0100

    homework react week3 uploaded

---
+++ react/week3/todo-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import AddTodo from "./components/addTodo";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Todo Application</h1>
+			<AddTodo />
+		</div>
+	);
+}
+
+export default App;
